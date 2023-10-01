518e7a12f6de5c7e52276849fdff81b4fd56bf47
Akash Gupta <75066582+Akash00028@users.noreply.github.com>
2023-10-01 18:20:19 +0530

    Update DepartmentController.java

--- advancedCrud/src/main/java/com/akash00028/advancedCrud/controller/DepartmentController.java
+++ advancedCrud/src/main/java/com/akash00028/advancedCrud/controller/DepartmentController.java
@@ -19,60 +19,36 @@ import com.akash00028.advancedCrud.models.Department;
 import com.akash00028.advancedCrud.service.DepartmentService;
 
 @RestController
-@RequestMapping("/api/v1/employees")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/employees")
 public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 
-	/**
-	 * http://localhost:9090/api/v1/employees/department
-	 * 
-	 * @param department
-	 * @return
-	 */
+	
 	@PostMapping("/department")
 	public ResponseEntity<Department> save(@RequestBody Department department) {
 		return departmentService.save(department);
 	}
 
-	/**
-	 * http://localhost:9090/api/v1/employees/department
-	 * 
-	 * @return
-	 */
+	
 	@GetMapping("/department")
 	public ResponseEntity<List<Department>> getSalaries() {
 		return departmentService.findAll();
 	}
 
-	/**
-	 * http://localhost:9090/api/v1/employees/department/123
-	 * 
-	 * @param id
-	 * @return
-	 */
+	
 	@GetMapping("/department/{id}")
 	public ResponseEntity<Department> getSalaryById(@PathVariable Long id) {
 		return departmentService.findById(id);
 	}
 
-	/**
-	 * @param id
-	 * @param department
-	 * @return
-	 */
+	
 	@PutMapping("/department/{id}")
 	public Department updateDepartment(@PathVariable Long id, @RequestBody Department department) {
 		return departmentService.update(id, department);
 	}
 
-	/**
-	 * http://localhost:9090/api/v1/employees/department/123
-	 * 
-	 * @param id
-	 * @return
-	 */
+	
 	@DeleteMapping("/department/{id}")
 	public ResponseEntity<Map<String, Boolean>> delete(@PathVariable Long id) {
 		return departmentService.delete(id);
